f54846e7cfe613d4369cf38ccf94a94f5070feab
unknown <honorineuwituze896@gmail.com>
2023-05-04 16:07:54 +0200

    displaying data for the each hospital according to the months

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,6 +7,8 @@ from .models import Hospital, Department, Profile, Hospital
 from .models import *
 from .models import Patient, Hospital
 from django.http import JsonResponse
+from collections import defaultdict
+import calendar
 
 def index(request):
     
@@ -37,23 +39,23 @@ def chart_data(request):
 
 # New way to get data
 
+
 def filter_inpatients_by_month(request):
     hospital_names = Hospital.objects.all()
-    print("these are the hospital names", hospital_names)
     data = {}
     for hospital_name in hospital_names:
         patient_hospital = Patient.objects.filter(hospital=hospital_name, status='inpatient')
         inpatient_data = patient_hospital.values_list('admission_date', flat=True)
-        inpatient_data = [date.strftime('%B') for date in inpatient_data]
-        inpatient_data_by_month = {}
-        for month in inpatient_data:
-            if month not in inpatient_data_by_month:
-                inpatient_data_by_month[month] = 0
+        inpatient_data_by_month = defaultdict(int)
+        for date in inpatient_data:
+            month = date.month
             inpatient_data_by_month[month] += 1
-        if hospital_name not in data:
+        months = [calendar.month_name[month] for month in range(1, 13)]
+        totals = [inpatient_data_by_month[month] for month in range(1, 13)]
+        if hospital_name.name not in data:
             data[hospital_name.name] = {}
-        data[hospital_name.name]['months'] = list(inpatient_data_by_month.keys())
-        data[hospital_name.name]['totals'] = list(inpatient_data_by_month.values())
+        data[hospital_name.name]['months'] = months
+        data[hospital_name.name]['totals'] = totals
     return JsonResponse(data)
 
 def signup(request):
--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -5,7 +5,7 @@ var chartData = {
   labels: ['Swing Bed', 'Acute Bed', 'inpatient', 'outpatient'],
   datasets: [{
     data: [0, 0, 0, 0],
-    backgroundColor: ['#FF6384', '#36A2EB', '#FF456', '#23A2AB'],
+    backgroundColor: ['#FF6384', '#36A2EB', '#463333', '#23A2AB'],
     borderWidth: 1,
   }]
 };
@@ -42,7 +42,7 @@ $.ajax({
 let lineData = {
   labels: [],
   datasets: [{
-    label: 'Inpatients',
+    label: [],
     data: [],
     borderWidth: 1,
     backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -50,7 +50,7 @@ let lineData = {
     hoverBackgroundColor: 'rgba(255, 99, 132, 0.4)',
     hoverBorderColor: 'rgba(255, 99, 132, 1)',
   }, {
-    label: 'Outpatients',
+    label: [],
     data: [],
     borderWidth: 1,
     backgroundColor: 'rgba(54, 162, 235, 0.2)',
@@ -59,15 +59,21 @@ let lineData = {
     hoverBorderColor: 'rgba(54, 162, 235, 1)',
   }],
 };
-
 $.ajax({
   url: '/linechart_data/',
   type: 'GET',
   dataType: 'json',
   success: function (response) {
-    lineData.labels = response.hospital_names;
-    lineData.datasets[0].data = response.inpatient_data;
-    lineData.datasets[1].data = response.outpatient_data;
+    // create an array of objects for each hospital
+    const datasets = Object.keys(response).map((hospital) => ({
+      label: hospital,
+      data: response[hospital]['totals'],
+      borderColor: getRandomColor(),
+      fill: false
+    }));
+    const months = response[Object.keys(response)[0]]['months'];
+    lineData.labels = months;
+    lineData.datasets = datasets;
     const ctx = document.getElementById('earning');
     new Chart(ctx, {
       type: 'line',
@@ -80,10 +86,25 @@ $.ajax({
               beginAtZero: true
             }
           }]
+        },
+        legend: {
+          display: true,
+          position: 'bottom',
+          labels: {
+            usePointStyle: true
+          }
         }
       }
     });
   }
 });
 
-
+function getRandomColor() {
+  // generate a random color for each hospital
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
