9ccd5f270bdcee19ed2f9cad3cd8f81e4b275867
alphonse SIBOMANA <alphonsesibomana@alphonses-MacBook-Pro.local>
2023-05-10 12:24:24 +0200

    added a profile pop up

--- static/dashboard/js/navigation.js
+++ static/dashboard/js/navigation.js
@@ -14,3 +14,49 @@ closeMenuIcon.addEventListener('click', () => {
     closeMenuIcon.style.display="none"
     menuIcon.style.display="block"
 })
+
+
+const rchDropDown = document.querySelector('#rhcDropDown')
+const laborDropDown = document.querySelector('#laborDropDown')
+const hspDropDown = document.querySelector('#hspDropDown')
+const rhcDropDownContent = document.querySelector('.rhcdropdown-content')
+const hspDropDownContent = document.querySelector('.hspdropdown-content')
+const laborDropDownContent = document.querySelector('.labordropdown-content')
+
+rchDropDown.addEventListener('click', () => {
+    rhcDropDownContent.classList.toggle("hidden")
+    rhcDropDownContent.classList.add("dropdown-content-class")
+
+})
+
+hspDropDown.addEventListener('click', () => {
+    hspDropDownContent.classList.toggle("hidden")
+    hspDropDownContent.classList.add("dropdown-content-class")
+})
+
+laborDropDown.addEventListener('click', () => {
+    laborDropDownContent.classList.toggle("hidden")
+    laborDropDownContent.classList.add("dropdown-content-class")
+})
+
+
+//profile popup
+profile = document.querySelector(".profile")
+profilePopup = document.querySelector(".profile-popup")
+closePopup = document.querySelector(".close-text")
+
+profile.addEventListener('click', () => {
+    profilePopup.style.visibility = "visible"
+})
+
+profilePopup.addEventListener("mouseover", () => {
+    profilePopup.style.visibility = "visible"
+})
+
+profilePopup.addEventListener("mouseout", () => {
+    profilePopup.style.visibility = "hidden"
+})
+
+closePopup.addEventListener('click', () => {
+    profilePopup.style.visibility = "hidden"
+})
