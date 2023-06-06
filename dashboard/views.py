f2103120d1738896bbfdde2a34ca1ca219006767
Charmant12 <sgcharmant@compstaffing.com>
2023-06-06 15:47:00 +0200

    removed the mortality dashboard on a single hospital view

--- dashboard/views.py
+++ dashboard/views.py
@@ -419,7 +419,7 @@ def singleHospital(request, hospital_id):
     total_acute_swing_bed_transfers = single_hospital_acute_swing_bed_transfers.aggregate(total_acute_swing_bed_transfers=models.Sum('acute_swing_bed_transfers'))['total_acute_swing_bed_transfers']
 
     measures_data = []
-    fields = ['readmissions','pressure_ulcer','discharges_home','emergency_room_transfers','acute_swing_bed_transfers','medication_errors','falls',
+    fields = ['mortality_rate', 'readmissions','pressure_ulcer','discharges_home','emergency_room_transfers','acute_swing_bed_transfers','medication_errors','falls',
               'against_medical_advice','left_without_being_seen','hospital_acquired_infection','covid_vaccination_total_percentage_of_compliance','complaint','grievances'
               ]
     
